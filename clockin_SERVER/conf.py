36c61f177d1a139bd8fab7731c99977899c6d351
ogios <2134692955@qq/com>
2022-10-20 11:19:12 +0800

    add SERVER version

---
+++ clockin_SERVER/conf.py
@@ -0,0 +1,279 @@
+# coding=utf-8
+
+import sqlite3
+import sys
+# import sys
+DEFAULT_DATA = {
+    "fxdj": "低风险地区",
+    "sfyz": "0",
+    "mqszd": "陕西省西安市`雁塔区东仪路8号西安欧亚学院",
+    "mqsxzd": "",
+    "xxdz": "",
+    "fxstzk": "",
+    "qtstyc": "无",
+    "jtdz": "",
+    "sfyxgl": "0",
+    "sfzs": "1",
+    "sfjchbr": "0",
+    "sfjjgl": "0",
+    "sfys": "0",
+    "sfqz": "0",
+    "sfbs": "0",
+    "mqcs": "",
+    "bz1": "",
+    "bz2": "",
+    "xm": "",
+    "xh": "",
+    "xb": "1",
+    "jtzz": "陕西省西安市",
+    "lxdh": "",
+    "rctw": "37",
+    "sffr": "0",
+    "sfjz": "0",
+    "sfks": "0",
+    "sffl": "0",
+    "sfjcwhry": "0",
+    "sffx": "1",
+    "fxrq": "",
+    "jtgj": "",
+    "cc": "",
+    "fdygh": "96012097",
+    "jcsj": "",
+    "jchbsj": "",
+    "mqsxdz": "西安欧亚学院"
+}
+
+# DEFAULT_ENCODING = sys.getdefaultencoding()
+data = {
+    "fxdq": "风险地区",
+    "mqszd": "目前所在地",
+    "xxdz": "详细地址",
+    "xm": "姓名",
+    "xh": "学号",
+    "jtzz": "家庭住址",
+    "lxdh": "联系电话",
+    "sfzs": "是否在陕西",
+    "mqsxdz": "目前陕西地址",
+    "rctw": "体温",
+}
+
+
+def color(string, color):
+    dic = {
+        'white': '\033[30m',
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'purple': '\033[35m',
+        'cyan': '\033[36m',
+        'black': '\033[37m'
+    }
+    return dic[color]+string+'\033[0m'
+
+
+MAIN_PAGE = '''
++----------------
+| 1. 添加用户
+| 2. 修改用户配置
+| 3. 删除用户
+| 4. 退出
++----------------
+>>> '''
+
+
+PAGE_2 = '''
++------------------
+| 1. 修改用户名与密码
+| 2. 修改用户打卡信息
+| 3. <-
+| 4. 退出
++------------------
+>>> '''
+
+
+MAIN_PAGE = color(MAIN_PAGE, "green")
+PAGE_2 = color(PAGE_2, "green")
+
+
+# print(options)
+# def form(options: str):
+#     options = options.split("\n")
+#     MAX_LENGTH = 0
+#     for i in options:
+#         if MAX_LENGTH < len(i):
+#             MAX_LENGTH = len(i)
+#     ht = "+"+"-"*(MAX_LENGTH+2)+"+"
+#     body = "\n".join(["| "+f"{i:<{MAX_LENGTH}}"+" |" for i in options])
+#     return ht+"\n"+body+"\n"+ht
+
+
+USER_INFO = "UserInfo"
+USER_DATA = "UserData"
+
+
+def addUser(uid, pwd):
+    res = getUser(uid)
+    if res:
+        db.execute(
+            f"update UserInfo set uid='{uid}', pwd='{pwd}' where uid='{uid}'")
+        conn.commit()
+        print("数据已更新")
+        return 1
+    else:
+        db.execute(f"insert into UserInfo(uid,pwd) values({uid},{pwd})")
+        print(db.fetchall())
+        conn.commit()
+        return 1
+
+
+def getUsers():
+    db.execute("select * from UserInfo")
+    return db.fetchall()
+
+
+def getUser(uid):
+    db.execute(f"select * from UserInfo where uid='{uid}'")
+    return db.fetchall()
+
+
+def deletUser(uid):
+    if not getUser(uid):
+        return 0
+    else:
+        db.execute(f"delete from UserInfo where uid='{uid}'")
+        if getData:
+            db.execute(f"delete from UserData where uid='{uid}'")
+        conn.commit()
+        return 1
+
+
+def getData(uid):
+    db.execute(f"select * from UserData where uid='{uid}'")
+    return db.fetchall()
+
+
+def InputUserInfo(isChange=True):
+    uid = input(color("学号:\n>>> ", "green"))
+    pwd = input(color("密码:\n>>> ", "green"))
+    if isChange:
+        if not getUser(uid):
+            while 1:
+                yn = input(color("用户不存在，是否创建(y/n)", "green"))
+                match yn:
+                    case "y":
+                        if addUser(uid, pwd):
+                            print("添加成功")
+                            return 1
+                        else:
+                            print("添加失败")
+                            return 0
+                    case "n":
+                        return 1
+                    case _:
+                        print("输入有误!")
+        else:
+            if addUser(uid, pwd):
+                print("添加成功")
+                return 1
+            else:
+                print("添加失败")
+                return 0
+    else:
+        if getUser(uid):
+            print("用户已存在")
+        else:
+            if addUser(uid, pwd):
+                print("添加成功")
+                addEmptyData(uid)
+                return 1
+            else:
+                print("添加失败")
+                return 0
+
+
+def ChangeUserData():
+    tmp = {}
+    res = input(color("学号:\n>>> ", "green"))
+    tmp["uid"] = res
+    tmp["xh"] = res
+    uid = res
+
+    for i in ["xm", "xxdz", "lxdh", "jtzz"]:
+        res = input(color(data[i]+":\n>>> ", "green"))
+        tmp[i] = res
+    if not getData(uid):
+        db.execute(
+            f"insert into UserData{str(tuple(tmp.keys()))} values{str(tuple(tmp.values()))}")
+    else:
+        set = "set "
+        for i in tmp:
+            set += f"{i}='{tmp[i]}',"
+        set = set[:-1]
+        db.execute(f"update UserData {set} where uid='{uid}'")
+    conn.commit()
+    return 1
+
+
+def addEmptyData(uid):
+    db.execute(f"insert into UserData(uid) values({uid})")
+    conn.commit()
+
+
+def main_ADDUSER():
+    if InputUserInfo(isChange=False):
+        while 1:
+            yn = input(color("是否继续修改已有/默认打卡配置(y/n)", "green"))
+            match yn:
+                case "y":
+                    ChangeUserData()
+                    return
+                case "n":
+                    return
+                case _:
+                    print("输入有误!")
+
+
+def main_DeleteUser():
+    uid = input(color("学号:\n>>> ", "green"))
+    if not deletUser(uid):
+        print("用户不存在")
+
+
+def page2():
+    while 1:
+        btn2 = input(PAGE_2)
+        match btn2:
+            case "1":
+                if InputUserInfo():
+                    return
+            case "2":
+                ChangeUserData()
+            case "3":
+                return
+            case "4":
+                sys.exit()
+            case _:
+                print("输入有误!")
+
+
+def main():
+    while 1:
+        btn1 = input(MAIN_PAGE)
+        match btn1:
+            case "1":
+                main_ADDUSER()
+            case "2":
+                page2()
+            case "3":
+                main_DeleteUser()
+            case "4":
+                sys.exit()
+            case _:
+                print("输入有误!")
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect("test")
+    db = conn.cursor()
+    main()
---
+++ clockin_SERVER/run.py
@@ -0,0 +1,227 @@
+import datetime
+from threading import Thread
+import requests
+import json
+import time
+import sqlite3
+
+from conf import DEFAULT_DATA, color
+
+
+global URL_LOGIN
+global URL_CLOCKIN
+global USER_DATA
+global USER_INFO
+global conn
+global db
+global OUT_OF_DATES
+URL_LOGIN = r"https://stu.eurasia.edu/yqsb/login/in"
+URL_CLOCKIN = "https://stu.eurasia.edu/yqsb/jkdj/save"
+USER_INFO = "UserInfo"
+USER_DATA = "UserData"
+OUT_OF_DATES = []
+
+
+def gettime():
+    return str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+
+def uop(string):
+    return f' [{gettime()}] - '+string
+
+
+def getUser(uid):
+    db.execute(f"select * from UserInfo where uid='{uid}'")
+    return db.fetchall()
+
+
+def getData(uid):
+    db.execute(f"select * from UserData where uid='{uid}'")
+    return db.fetchall()
+
+
+def getColumns(table):
+    db.execute(f"pragma table_info({table})")
+    return [i[1] for i in db.fetchall()]
+
+
+def putToken(uid, token):
+    print(uid)
+    print(token)
+    db.execute(f"update UserInfo set token='{token}' where uid='{uid}'")
+    conn.commit()
+
+
+def getSQLToken(uid):
+    db.execute(f"select token from UserInfo where uid='{uid}'")
+    return db.fetchall()[0][0]
+
+
+class stu:
+    def __init__(self, uid):
+        self.uid = uid
+        self.pwd = None
+        self.token = None
+        self.tokenTime = None
+        self.headers = None
+        self.userdata = None
+        self.defaultdata = None
+        self.data = None
+        self.isClockin = None
+        self.isClockinTime = None
+        self.isTokenCheck = False
+
+    def initData(self):
+        if getUser(self.uid) and getData(self.uid):
+            # print(getUser(self.uid))
+            self.pwd = getUser(self.uid)[0][2]
+            self.data = DEFAULT_DATA.copy()
+            self.userdata = getData(self.uid)[0][2:]
+            cols = getColumns(USER_DATA)[2:]
+            for i in range(len(cols)):
+                self.data[cols[i]] = self.userdata[i]
+            token = getSQLToken(self.uid)
+            if not token:
+                self.getToken()
+            else:
+                self.token = token
+                self.headers = {
+                    "Authorization": token,
+                    "Content-Type": "application/json;",
+                    "token": token,
+                }
+            return self
+        else:
+            self.print(self.uid+"用户不存在")
+            return None
+
+    def print(self, string) -> str:
+        print(self.uid+" - "+string)
+
+    def getToken(self):
+        global URL_LOGIN
+        params = {
+            "zh": self.uid,
+            "mm": self.pwd,
+        }
+
+        _token = requests.get(URL_LOGIN, params=params)
+
+        if _token.status_code == 200:
+            res = _token.json()
+            if res["success"]:
+                token = res["token"]
+                self.token = token
+                self.tokenTime = time.strftime(
+                    "%Y-%m-%d", time.localtime(time.time()))
+                self.headers = {
+                    "Authorization": token,
+                    "Content-Type": "application/json;",
+                    "token": token,
+                }
+                putToken(self.uid, token)
+                self.print("token请求成功")
+                return 1
+            else:
+                self.print("token请求出错 - "+res["msg"])
+                return 0
+
+        else:
+            self.print("请求", res.status_code)
+        return 0
+
+    def clockIn(self):
+
+        if self.token:
+            if self._clockIn() == 1:
+                self.print("已打卡")
+            else:
+                self.print("打卡失败")
+
+    def _clockIn(self):
+        global URL_CLOCKIN
+        res = requests.post(URL_CLOCKIN, headers=self.headers,
+                            data=json.dumps(self.data))
+        js = res.json()
+        if res.status_code == 200:
+            if js["success"] == True:
+                self.changeClockin(1)
+                return 1
+            else:
+                if "登陆过期" in js["msg"]:
+                    self.print("登陆过期，等待重新申请token")
+                    OUT_OF_DATES += [self]
+                    return 0
+                    # if self.isTokenCheck:
+                    #     return 0
+                    # else:
+                    #     self.isTokenCheck = True
+                    #     if self.getToken() == 1:
+                    #         return self._clockIn()
+
+                elif "今日已登记" in js["msg"]:
+                    self.print(js["msg"])
+                    self.changeClockin(1)
+                    return 1
+                else:
+                    self.print(js["msg"])
+                    return 0
+        else:
+            self.print("POST请求失败 - "+res.text)
+            return 0
+
+    def changeClockin(self, isclock):
+        if isclock == 1:
+            self.isClockin = True
+            self.isClockinTime = time.strftime(
+                "%Y-%m-%d", time.localtime(time.time()))
+
+
+def getUsers():
+    db.execute("select * from UserInfo")
+    return db.fetchall()
+
+
+def createUsers(uids):
+    users = []
+    for i in uids:
+        user = stu(i).initData()
+        if user:
+            users += [user]
+    return users
+
+
+def ClockIn(user: stu):
+    user.clockIn()
+
+
+def main():
+    uids = [i[1] for i in getUsers()]
+    users = createUsers(uids)
+    # for i in uids:
+    #     user = createUser()
+    #     if user:
+    #         users += [user]
+    ths = []
+    for i in users:
+        th = Thread(target=ClockIn, args=(i,))
+        th.daemon = True
+        th.start()
+        ths += [th]
+    for i in ths:
+        i.join()
+    for i in OUT_OF_DATES:
+        if i.getToken():
+            i.clockIn()
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect("test", check_same_thread=False)
+    db = conn.cursor()
+    while 1:
+        print(uop("时间检查"))
+        TIME_NOW = time.strftime("%H", time.localtime(time.time()))
+        if TIME_NOW == "6" or TIME_NOW == "7":
+            print(uop(color("开始打卡", "green")))
+            main()
+        time.sleep(3600)
